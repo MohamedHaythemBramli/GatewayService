186691cb03648dbfe7c16a3d8ab1eb227d801c06
MohamedHaythemBramli <mohamedhaythembramli90@gmail.com>
2022-04-01 14:01:10 +0000

    Update Configuartion to use dynamic microservices enregistration with localhost:8888/MICROSERVICE-NAME/endpoints

--- src/main/java/com/bramli/gatewayservice/configuration/RoutesConfiguration.java
+++ src/main/java/com/bramli/gatewayservice/configuration/RoutesConfiguration.java
@@ -1,6 +1,9 @@
 package com.bramli.gatewayservice.configuration;
 
 
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RoutesConfiguration {
-    @Bean
+
+    /*@Bean
     RouteLocator routeLocator (RouteLocatorBuilder builder){
         return builder.routes()
                 .route((r)->r.path("r1","/customers/**")
@@ -16,6 +20,9 @@ public class RoutesConfiguration {
                 .route((r)->r.path("r2","/products/**")
                         .uri("lb://inventory")).build();
 
+    }*/
+    @Bean
+   DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
     }
-
 }
